2b5698de74613e941a61b6b16bef07b857ff1aff
vidhi kiran <vidhi.kiran.mec19@itbhu.ac.in>
2021-03-27 18:25:05 +0530

    adding new files

---
+++ src/components/CardList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) =>{
+	return (
+		<div>
+			{robots.map((user, i) =>{
+			return <Card key = {i}
+			name = {user.name} 
+			email = {user.email} 
+			id = {user.id}/>
+		})} 
+		</div>
+	);
+}
+
+export default CardList;
